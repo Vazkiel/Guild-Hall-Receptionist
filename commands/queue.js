c0499413ce9be4f61de5839504d5337962a51913
Vazkiel <vazkielgaming@gmail.com>
2020-09-14 11:25:29 -0400

    initial

---
+++ commands/queue.js
@@ -0,0 +1,12 @@
+exports.run = async (client, message, args, ops) => {
+    let fetched = ops.active.get(message.guild.id);
+
+    if(!fetched) return message.channel.send("There currently isn\'t any music playing in this guild.");
+
+    let queue = fetched.queue;
+    let nowPlaying = queue[0];
+    
+    let resp = `__**Now playing:**__\n**${nowPlaying.songTitle}** -- **Requested by:** *${nowPlaying.requester}*\n\n__**Queue****__`;
+
+    //for(var i = 1; i < )
+}
