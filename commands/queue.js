c44aae86abbd0c36613a093034456977a0322c0d
Vazkiel <vazkielgaming@gmail.com>
2020-09-14 12:29:45 -0400

    Initial

--- commands/queue.js
+++ commands/queue.js
@@ -6,7 +6,11 @@ exports.run = async (client, message, args, ops) => {
     let queue = fetched.queue;
     let nowPlaying = queue[0];
     
-    let resp = `__**Now playing:**__\n**${nowPlaying.songTitle}** -- **Requested by:** *${nowPlaying.requester}*\n\n__**Queue****__`;
+    let resp = `__**Now playing:**__\n**${nowPlaying.songTitle}** —— **Requested by:** *${nowPlaying.requester}*\n\n__**Queue**__\n`;
 
-    //for(var i = 1; i < )
+    for(var i = 1; i < queue.length; i++) {
+        resp += `${i}. **${queue[i].songTitle}** —— **Requested by:** *${queue[i].requester}*\n`;
+    }
+
+    message.channel.send(resp);
 }
