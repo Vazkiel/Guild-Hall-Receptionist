fccf59d94c0100a51a5c00ca1b8f2081de804f8d
Vazkiel <vazkielgaming@gmail.com>
2020-09-14 03:59:45 -0400

    Testing configuration

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async() => {
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("Prefix is '>'", {type: "WATCHING"});
+    bot.user.setActivity("Prefix is '>'", {type: "STREAMING"});
 });
 
 bot.login(config.token);
