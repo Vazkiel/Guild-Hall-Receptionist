8eb3c9974720c7847d9f31413dfcb244ed9e49f2
Vazkiel <vazkielgaming@gmail.com>
2020-09-14 04:11:39 -0400

    Changes

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", async() => {
-    console.log(`${bot.user.username} is online`)
+const fs = require('fs');
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    bot.commands.set(command.name, command);
+}
+
+bot.on('ready', () => {
+    console.log(`${bot.user.username} is online`);
     bot.user.setActivity("Prefix is '>'", {type: "STREAMING"});
+});
+
+bot.on('message', message => {
+    let args = message.content.substring(config.prefix.length).split(" ");
+
+    switch (args[0]) {
+        case "ping":
+            bot.commands.get('ping').execute(message, args);
+        break;
+    }
 });
 
 bot.login(config.token);
